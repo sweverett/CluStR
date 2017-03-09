2de7a8ca3fc578407f13993d0d134285b15760ab
Spencer Everett <spencerweverett@gmail.com>
2017-03-08 20:31:14 -0800

    Completed the `plot_residuals()` method. Changed some plotting title options throughout. closes #5

--- plotlib.py
+++ plotlib.py
@@ -1,6 +1,6 @@
 # Plotting library for CluStR
 
-from clustr import fits_label
+from clustr import fits_label, scale
 import matplotlib
 matplotlib.use('Agg')
 import PyPDF2, corner, os
@@ -64,9 +64,6 @@ def plot_scatter(options, data_obs, kelly_scaled_fit, mantz_scaled_fit,x_piv, x_
     # Plot data
     plt.errorbar(x_obs, y_obs, xerr=x_err_obs, yerr=y_err_obs, c='r', fmt='o')
 
-    # OLD: now replaced by METHODS
-    #methods = clustr.return_methods_list(options.method)
-
     for method in METHODS:
         if method == 'kelly':
             scaled_fit = kelly_scaled_fit
@@ -79,6 +76,8 @@ def plot_scatter(options, data_obs, kelly_scaled_fit, mantz_scaled_fit,x_piv, x_
         (fit_int, fit_slope, fit_sig) = scaled_fit
         data_fit = scaled_fit_to_data(x_min, x_max, x_piv, scaled_fit)
         (x_fit, y_fit, _, _) = data_fit
+
+        print('mean b, m, sig: {}, {}, {}'.format(np.mean(fit_int),np.mean(fit_slope),np.mean(fit_sig)))
 
         # plot fit
         if PARAMETERS['show_method_name'] is True or len(METHODS) > 1:
@@ -127,6 +126,72 @@ def plot_scatter(options, data_obs, kelly_scaled_fit, mantz_scaled_fit,x_piv, x_
 
     return
 
+def plot_residuals(options, data_obs, kelly_scaled_fit, mantz_scaled_fit):
+    '''
+    FIX: Description
+    '''
+
+    #(x_obs, y_obs, x_err_obs, y_err_obs) = data_obs
+    (lx_obs, ly_obs, lx_err_obs, ly_err_obs, x_piv) = scale(*data_obs)
+
+    for method in METHODS:
+        if method == 'kelly':
+            scaled_fit = kelly_scaled_fit
+        elif method == 'mantz':
+            scaled_fit = mantz_scaled_fit
+        else:
+            print "WARNNG: Only `kelly`, `mantz`, or `both` are valid method options. Will use Kelly method instead."
+            scaled_fit = kelly_scaled_fit
+
+        (B, M, S) = scaled_fit
+
+        b, m, sig = np.mean(B), np.mean(M), np.mean(S)
+
+        # Calculate residuals
+        x_fit = lx_obs
+        y_fit = m*x_fit+b
+        #residuals = (ly_obs - y_fit) / ly_err_obs
+        residuals = (ly_obs - y_fit) / np.std(ly_obs)
+
+        '''
+        # Make residual plot
+        fig1 = plt.figure(1)
+        #Plot Data-model
+        frame1 = fig1.add_axes((.1,.3,.8,.6))
+        #xstart, ystart, xend, yend [units are fraction of the image frame, from bottom left corner]
+        plt.errorbar(lx_obs, ly_obs, xerr=lx_err_obs, yerr=ly_err_obs, c='r', fmt='o')
+        plt.plot(x_fit,y_fit,'b') #Best fit model
+        frame1.set_xticklabels([]) #Remove x-tic labels for the first frame
+
+        #Residual plot
+        frame2 = fig1.add_axes((.1,.1,.8,.2))
+        plt.plot(lx_obs,residuals,'ob')
+        plt.plot([np.min(lx_obs),np.max(lx_obs)],[0,0],'k--',linewidth=2)
+
+        plt.title('{} Method'.format(method.capitalize()),fontsize=14)
+        '''
+
+        fig = plt.figure()
+
+        # Bin number
+        # FIX: make bins automatically consistent with Michigan group
+        nbin = 18
+
+        plt.hist(residuals,nbin)
+        plt.xlabel(r'$\Delta(\ln X)/\sigma_{\ln X}$',fontsize=16)
+        plt.ylabel('Count',fontsize=16)
+
+        if PARAMETERS['show_method_name'] is True or len(METHODS) > 1:
+            plt.title('{} Residuals for {} Method'.format(fits_label(options.y),method.capitalize()),fontsize=16)
+        else:
+            plt.title('{} Residuals'.format(fits_label(options.y)),fontsize=16)
+
+
+        plt.savefig('Residuals-{}-{}{}-{}.pdf'.format(method,options.prefix, fits_label(options.y), fits_label(options.x)))
+
+    return
+
+
 def plot_corners(options, kelly_scaled_fit, mantz_scaled_fit):
     '''
     Makes corner plots for the desired Kelly and/or Mantz method parameter posteriors. Burn is the
@@ -135,11 +200,7 @@ def plot_corners(options, kelly_scaled_fit, mantz_scaled_fit):
 
     burn = PARAMETERS['burn']
 
-    # FIX: add a condition to adapt burn # if needed
-
-    # OLD: now uses METHODS
-    #methods = clustr.return_methods_list(options.method)
-
+    # FIX: Is this still being used?
     N = np.size(METHODS) # Number of subplots
     n = 1 # Subplot counter
 
@@ -182,7 +243,10 @@ def plot_corners(options, kelly_scaled_fit, mantz_scaled_fit):
                         show_titles=True, title_args={"fontsize": 18},
                         plot_datapoints=True, fill_contours=True, levels=[0.68, 0.95], color='b', bins=40, smooth=1.0);
 
-        fig.suptitle('{} Method'.format(method.capitalize()),fontsize=16)
+        if PARAMETERS['show_method_name'] is True or len(METHODS) > 1:
+            fig.suptitle('Posterior for {} Method'.format(method.capitalize()),fontsize=16)
+        else:
+            fig.suptitle('Posterior',fontsize=16)
 
         plt.savefig('Corner-{}-{}{}-{}.pdf'.format(method,options.prefix, fits_label(options.y), fits_label(options.x)))#,bbox_inches='tight')
 
@@ -286,6 +350,12 @@ def make_plots(options, parameters, methods, data_obs, kelly_scaled_fit, mantz_s
         # Add scatter/fit plot
         pdfs.append('Scatter-{}{}-{}.pdf'.format(options.prefix, fits_label(options.y), fits_label(options.x)))
 
+    if PARAMETERS['residuals'] is True:
+        plot_residuals(options, data_obs, kelly_scaled_fit, mantz_scaled_fit)
+        # Add residual plot(s)
+        for method in METHODS:
+            pdfs.append('Residuals-{}-{}{}-{}.pdf'.format(method,options.prefix, fits_label(options.y), fits_label(options.x)))
+
     if PARAMETERS['corner'] is True:
         plot_corners(options,kelly_scaled_fit,mantz_scaled_fit)
         # Add corner plot(s)
@@ -297,10 +367,6 @@ def make_plots(options, parameters, methods, data_obs, kelly_scaled_fit, mantz_s
         # Add chain plot(s)
         for method in METHODS:
             pdfs.append('Chains-{}-{}{}-{}.pdf'.format(method,options.prefix, fits_label(options.y), fits_label(options.x)))
-
-    if PARAMETERS['residuals'] is True:
-        # FIX: Implement!
-        pass
 
     merger = PyPDF2.PdfFileMerger()
 
