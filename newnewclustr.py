28f781ecbef14ffad1532f1ee81edd7e1623dc33
Paige <paigemkelly32@gmail.com>
2020-02-07 05:38:22 -0800

    group meeting edits

--- newnewclustr.py
+++ newnewclustr.py
@@ -1,55 +1,97 @@
-import argparse as args
+from argparse import ArgumentParser
 import os
 #import cPickle as pickle
-import astropy.io.fits as fits
+from astropy.table import Table
 import numpy as np
 #import reglib  # Regression library
-#import plotlib
+import matplotlib.pyplot as plt
+parser = ArgumentParser()
+parser.add_argument(
+'config_file',
+type = str,
+help = 'the filename of the desired config to run'
+)
+parser.add_argument(
+'cat_file',
+type = str,
+help = 'the filename of the desired catalog to open'
+)
 
 class Config:
     def __init__(self,config_filename):
-        pass
+        self.config_filename = config_filename
+        return
 class Catalog:
     #read/load the fits table that contains the data
-    def __init__(self,fits_file):
-        self.fits = fits_file
-        pass
+    def __init__(self,cat_file_name,config):
+        self.file_name = cat_file_name
+
+        #self.property = config.property # for example
+        #,,,
+
+        self._load_catalog()
+
+        return
+
+    def _load_catalog(self):
+        self.table = Table.read(self.file_name)
+
+        # could do other things...
+
+        return
+
+    def plot_data(self, xcol, ycol, size=8, ylog=False):
+        x = self.table[xcol]
+        y = self.table[ycol]
+
+        plt.scatter(x, y)
+        plt.xlabel(xcol)
+        plt.ylabel(ycol)
+
+        if ylog is True:
+            plt.yscale('log')
+        plt.gcf().set_size_inches(size, size)
+        plt.show()
+
+        return
+
 class Flag:
-    def __init__(self,data):
-        self.data = data
-        pass
+    def __init__(self,catalog):
+        self.catalog = catalog
+        return
 class Data:
     #take data frome the Catalog Class and pick rows and columns we want to fit
-    def __init__(self,data,flags):
-        self.data = data
-        self.flags = flags
-        pass
+    #dont call flag in main call it here
+    def __init__(self,config, catalog):
+        self.config = config
+        self.catalog = catalog
+        return
 class Fitter:
-    def __init__(self,data_set):
-        self.data_set = data_set
+    def __init__(self,viable_data):
+        self.viable_data= viable_data
+        return
     def fit(self):
         pass
 
 
 def main():
 
+    args = parser.parse_args()
 #what is "run_options" should it be an input of Config?
-    config_filename = 'param.config'
+    config_filename = args.config_file
 
-    config_file = Config(config_filename) #(2)
+    config = Config(config_filename) #(2)
 
-    file_name = fits.open("y3a2-6.4.22+2_peak_fixed_maybe_final_merged(2).fits")
+    cat_file_name = args.cat_file
 
-    full_data_set = Catalog(file_name) #(3)
+    catalog = Catalog(cat_file_name, config) #(3)
 
-    flagged_data = Flag(full_data_set) #(4)
+    viable_data = Data(config, catalog) #(4)
 
-    viable_data = Data(full_data_set,flagged_data) #(4)
+    fit = Fitter.fit(viable_data) #(6)
 
-    Fitter.fit(viable_data) #(6)
-
-
-
+    # Just for fun!
+    catalog.plot_data('lambda', 'r500_band_lumin', ylog=True)
 
 if __name__ == '__main__':
 
