4186bd39ccab2d81d57ce040403fc68f3fe0d801
Paige <paigemkelly32@gmail.com>
2020-02-24 01:59:04 -0800

    combining 2 codes

--- newnewclustr.py
+++ newnewclustr.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 
 class ArgumentParser:
     def __inti__(self,):
-    
+
     def parse_options(self):
 
     parser = ArgumentParser()
@@ -26,9 +26,10 @@ class Config:
     _required_keys=[]
     _default_run_name = 'clustr'
     def __init__(self,config_file, run_options):
+
         with open(config_file, 'r') as stream:
 
-            self.config_file = yaml.safe_load(stream)      
+            self.config_file = yaml.safe_load(stream)
 
         self.run_options = run_options
         if run_options.run_name is None:
@@ -39,22 +40,22 @@ class Config:
 
     def __getitem__(self,key):
         return self._config.__dict__[key]
-    
+
     def __setitem(self, key, value):
         self._config._dict_[key] = value
-    
+
     def __delitem__(self,key):
         del self._config.__dict__[key]
-    
+
     def __contain__(slef, key):
         return key in seelf._config.__dict__
 
     def __len__(self):
-        return len(self._config.__dict__) 
-    
+        return len(self._config.__dict__)
+
     def __repr__(self):
         return repr(self._config.__dict__)
-    
+
     def run_name()
 pass
 
@@ -91,7 +92,11 @@ class Catalog:
         plt.show()
 
         return
-
+    def Ez(z)
+        Om = 0.3
+        H_0 = 0.7
+        h = H_0/100
+        return np.sqrt(Om*(1.+z)**3 + h
 #class Flag:
     #def __init__(self,catalog):
         #self.catalog = catalog
@@ -109,6 +114,60 @@ class Data:
     def open_catalog(self): #run Catalog's _load_catolog to have to table in this class
         table_cat = catalog._load_catolog
         return table_cat
+    def get_data(self):
+        #hdulist = fits.open(options.catalog)
+        #data = hdulist[1].data
+        #
+        label_x = self.run_options[x] #how to access ?
+        label_y = self.run_options[y] #
+        x = self.data[label_x] #How to access?
+        y = self.data[label_y] #how to access? 
+
+        # Number of original data
+        N = np.size(x)
+        #where does the l come from? should it also be lambda?
+        if label_x[0] == 'l' and label_y != 'lambda':
+            x /= Catalog.Ez(data['redshift'])
+        if label_y[0] == 'l' and label_x != 'lambda':
+            y /= Catalog.Ez(data['redshift'])
+ERROR STUFF
+
+        flags = self.run_options[flags] #change. ?
+        if flags is not None:
+            # FIX: Should be more error handling than this!
+            # FIX: Should write method to ensure all the counts are what we expect
+
+            mask = f.create_cuts(self)
+            self.x[mask] = -1
+            self.y[mask] = -1
+
+            print (
+                'NOTE: `Removed` counts may be redundant, '
+                'as some data fail multiple flags.'
+            )
+
+            # Take rows with good data, and all flagged data removed
+            good_rows = np.all([x != -1, y != -1], axis=0)
+            x = x[good_rows]
+            y = y[good_rows]
+            x_err = x_err[good_rows]
+            y_err = y_err[good_rows]
+            print 'Accepted {} data out of {}'.format(np.size(x), N)
+        if np.size(x) == 0:
+            print (
+                '\nWARNING: No data survived flag removal. '
+                'Suggest changing flag parameters in `param.config`.'
+                '\n\nClosing program...\n'
+            )
+            raise SystemExit(2)
+
+        print 'mean x error:', np.mean(x_err)
+        print 'mean y error:', np.mean(y_err)
+
+        hdulist.close()
+
+        return (x, y, x_err, y_err) #put all into 1 and return 'd'
+
     #identify Flags
     #take flagged data out of table_cat
     #return only viable data
